15bf8c8234ba9ffbc7b1e9a97d1481093ca4c008
ibtixam <m.ibtisam868@gmail.com>
2024-01-11 12:38:16 +0500

    Validate user routes and make a login request

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -20,6 +20,24 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  res.status(200).send("Login");
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await Users.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ error: "Invalid credentials" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(400).json({ error: "Invalid credentials" });
+    }
+
+    res.status(200).json({ message: "Login successful" });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import { connectToDb } from "./db.js";
 import { addProducts, getProducts } from "./controllers/productControllers.js";
 import { login, register } from "./controllers/userControllers.js";
+import { body } from "express-validator";
 
 const app = express();
 const PORT = 5000;
@@ -15,8 +16,22 @@ app.post("/api/add/products", addProducts);
 app.get("/api/get/products", getProducts);
 
 // Auth Routes
-app.post("/login", login);
-app.post("/register", register);
+app.post(
+  "/login",
+  body("name", "Name must be alleast 3 characters").isLength({ min: 3 }),
+  body("email", "Please Enter an valid email").isEmail(),
+  body("password", "Name must be alleast 6 characters").isLength({ min: 6 }),
+  login
+);
+app.post(
+  "/register",
+  [
+    body("name", "Name must be alleast 3 characters").isLength({ min: 3 }),
+    body("email", "Please Enter an valid email").isEmail(),
+    body("password", "Name must be alleast 6 characters").isLength({ min: 6 }),
+  ],
+  register
+);
 
 app.listen(PORT, () => {
   console.log(`Backend listening on http://localhost:${PORT}`);
