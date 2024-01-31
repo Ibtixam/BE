3582ba342d117456223a0ce9aee7fa9729d6256f
Ibtixam <m.ibtisam868@gmail.com>
2024-01-31 19:14:11 +0500

    Add MongoDb Cloud Url

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectToDb = () => {
-  mongoose.connect("mongodb://localhost:27017/BE");
+  mongoose.connect(process.env.MONGO_URL);
 
   const db = mongoose.connection;
 
